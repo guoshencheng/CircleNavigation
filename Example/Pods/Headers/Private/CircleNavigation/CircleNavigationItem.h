1224b4e309ab8762a55eea755557ffcd21b65a9e
guoshencheng <648772021@qq.com>
2016-03-15 15:12:31 +0800

    delete pod

--- Example/Pods/Headers/Private/CircleNavigation/CircleNavigationItem.h
+++
@@ -1,1 +0,0 @@
-../../../../../Pod/Classes/CircleNavigationItem.h
--- Example/Pods/Target Support Files/CircleNavigation/CircleNavigation-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CircleNavigation : NSObject
-@end
-@implementation PodsDummy_CircleNavigation
-@end
